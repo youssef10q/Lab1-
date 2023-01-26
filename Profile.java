f0bee22b34598e8c6822c46156f2cfe5d2bea137
Youssef aoun <youssefaoun48@gmail.com>
2023-01-26 20:25:27 +0200

    initial info provided

---
+++ Profile.java
@@ -0,0 +1,7 @@
+public class Profile{
+	public static void main(String[] args){
+		System.out.println("Youssef Aoun");
+		System.out.println("Mechatronics");
+		System.out.println("1st year");
+	}
+}
