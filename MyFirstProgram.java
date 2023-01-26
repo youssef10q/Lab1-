c7cb1e2e2bd61648c3c959230e98cc828130def8
Youssef aoun <youssefaoun48@gmail.com>
2023-01-26 19:22:01 +0200

    My First Program is done

---
+++ MyFirstProgram.java
@@ -0,0 +1,9 @@
+public class MyFirstProgram
+{
+	public static void main(String[] args){
+		System.out.print("This is my ");
+		System.out.println("first java program");
+		System.out.println("Programming is fun!");
+	}
+
+}
