1f607ff6e6781e62264605376a66563f9bccb524
Youssef aoun <youssefaoun48@gmail.com>
2023-01-26 20:18:57 +0200

    Added problem file and corrected them

---
+++ Problem.java
@@ -0,0 +1,8 @@
+public class Problem {
+	public static void main(String[] args) {
+		System.out.print("Hi, can we chack this?");
+		System.out.println("It has a lot of problems");
+		System.out.print("Yeah, I know !!!");
+		//This is a comment
+	}
+}
